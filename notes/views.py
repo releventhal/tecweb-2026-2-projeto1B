c79d64f3e5ef4473cedc826817d9f2f499f13a10
releventhal <renatahl@al.insper.edu.br>
2026-09-08 08:54:58 -0300

    Handout parte 6

--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Note
 
 
 def index(request):
-    all_notes = Note.objects.all()
-    print(all_notes)
-    return render(request, 'notes/index.html', {'notes': all_notes})
+    if request.method == 'POST':
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+        # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
+        Note.objects.create(title=title, content=content)
+        return redirect('index')
+    else:
+        all_notes = Note.objects.all()
+        return render(request, 'notes/index.html', {'notes': all_notes})
