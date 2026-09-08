1a7a1c2f661ac6cca7939a9b7afa71ecbe17de41
releventhal <renatahl@al.insper.edu.br>
2026-09-08 08:38:07 -0300

    Handout parte 5

--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Note
 
 
 def index(request):
-    return HttpResponse("Olá mundo! Este é o app notes de Tecnologias Web do Insper.")
+    all_notes = Note.objects.all()
+    print(all_notes)
+    return render(request, 'notes/index.html', {'notes': all_notes})
